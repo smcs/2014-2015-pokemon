3b39827f6a2756319abd3c229827ad86b2dc50c8
Niles Stanton <NilesStanton@stmarksschool.org>
2015-02-23 09:31:40 -0500

    Pokemon Variables

--- Pokemon/src/Pokemon.java
+++ Pokemon/src/Pokemon.java
@@ -1,6 +1,15 @@
 
 public class Pokemon {
 	
-	public void Pokemon(int Bhp, int Hiv, int Hev, int Batk, int Atkiv, int Bdef, int BSpAtk, int BSpDef, int Bs){
-
+	public void Pokemon (int Species, String Name, int Ability, int Nature, int HeldItem, int Level, int Exp,
+			int Type1, int Type2, int Status, boolean confused, int currentHealth, boolean Gender,
+			Move Move1, Move Move2, Move Move3, Move Move4, 
+			int HealthBase, int Hiv, int Hev, 
+			int AtkBase, int Atkiv, int Atkev,
+			int DefBase, int Defiv, int Defev,
+			int SpAtkBase, int SpAtkiv, int SpAtkev,
+			int SpDefBase, int SpDefiv, int SpDefev,
+			int SpeedBase, int Speediv, int Speedev)
+	{}
+	
 }
