e4018809dd154c768e2596111f2a7a036e5d5a62
alexvc <alexvoncampe@stmarksschool.org>
2015-02-24 10:35:51 -0500

    Title Page Changes

--- Pokemon/src/PokemonController.java
+++ Pokemon/src/PokemonController.java
@@ -15,11 +15,12 @@ public class PokemonController extends WindowController{
 		background.setColor(new Color(205, 205, 205));
 		
 		FramedRect battleSim = new FramedRect(50, 300, 300, 80, canvas);
-		//Text BattleSim = new Text("Battle Simulator", 40, 300, canvas);
-		
-		
+		Text battleSimText = new Text("Battle Simulator", 80, 320, canvas);
+		battleSimText.setFontSize(32);
 		
 		FramedRect campaign = new FramedRect(width - 350, 300, 300, 80, canvas);
+		Text campaignText = new Text("Campaign", 470, 320, canvas);
+		campaignText.setFontSize(32);
 		
 	}
 	
