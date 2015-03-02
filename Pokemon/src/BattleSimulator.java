956ba1d148673c7cd4b3b9ea8c53e2878d2090b6
alexvc <alexvoncampe@stmarksschool.org>
2015-03-02 09:51:28 -0500

    Work on PokemonController and added BattleSim Class

--- Pokemon/src/BattleSimulator.java
+++ Pokemon/src/BattleSimulator.java
@@ -3,9 +3,12 @@ import objectdraw.*;
 public class BattleSimulator {
 
 	public BattleSimulator(){
+
+		
 		
 		//set player 1 pokemon and player 2 pokemon
-			//
+			//Pokemon Name1 = new Pokemon(blah blah);
+			//Pokemon Name2 = new Pokemon(blah blah);
 		
 		
 	}
--- Pokemon/src/PokemonController.java
+++ Pokemon/src/PokemonController.java
@@ -1,18 +1,20 @@
 import objectdraw.*;
+import javax.swing.*;
 import java.awt.*;
 
 public class PokemonController extends WindowController{
 
 	int width = 750;
 	int height = 500;
+
+	String P1Name, P2Name;
 	
 	public void begin() {
 
 		setSize(width, height);
 		canvas.clear();
-		
-		FilledRect background = new FilledRect(0, 0, width, height, canvas);
-		background.setColor(new Color(205, 205, 205));
+	
+		((Component) canvas).setBackground(new Color(205, 205, 205));
 		
 		FramedRect battleSim = new FramedRect(50, 300, 300, 80, canvas);
 		Text battleSimText = new Text("Battle Simulator", 80, 320, canvas);
@@ -21,6 +23,36 @@ public class PokemonController extends WindowController{
 		FramedRect campaign = new FramedRect(width - 350, 300, 300, 80, canvas);
 		Text campaignText = new Text("Campaign", 470, 320, canvas);
 		campaignText.setFontSize(32);
+		
+	}
+	
+	public void onMouseClick(Location point){
+		
+		if(point.getX() > 50  && point.getX() < 350 && 
+				point.getY() > 300 && point.getY() < 380){
+			setP1Name();
+			setP2Name();
+			drawBSmain();
+			BattleSimulator bs = new BattleSimulator();
+		}
+		
+	}
+	
+	public void setP1Name(){
+		
+		P1Name =  JOptionPane.showInputDialog(null, "Player 1 Name:");
+	    
+	}
+	public void setP2Name(){
+		
+		P2Name =  JOptionPane.showInputDialog(null, "Player 2 Name:");
+	    
+	}
+
+	private void drawBSmain() {
+		
+		canvas.clear();
+		
 		
 	}
 	
