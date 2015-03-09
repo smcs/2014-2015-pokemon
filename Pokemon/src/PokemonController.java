05a62dd55e96e6c46ab9859cbc148221c369baf9
Niles Stanton <NilesStanton@stmarksschool.org>
2015-03-09 09:12:13 -0400

    remove anotations

--- Pokemon/src/PokemonController.java
+++ Pokemon/src/PokemonController.java
@@ -76,31 +76,27 @@ public class PokemonController extends WindowController implements MouseListener
 		
 	}
 
-	@Override
+
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
