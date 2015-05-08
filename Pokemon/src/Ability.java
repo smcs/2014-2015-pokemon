acb504b5df6d621bd02194bf24f0111c48ccdac3
Niles Stanton <NilesStanton@stmarksschool.org>
2015-05-08 09:41:46 -0400

    Added Gen 3 abilities

--- Pokemon/src/Ability.java
+++ Pokemon/src/Ability.java
@@ -4,7 +4,96 @@ public class Ability {
 	public Ability(){
 		
 	}
+	public static Ability Stench;
+	public static Ability Drizzle;
+	public static Ability speedBoost;
+	public static Ability battleArmor;
+	public static Ability Sturdy;
+	
+	public static Ability Damp;
+	public static Ability Limber;
+	public static Ability sandVeil;
 	public static Ability Static;
+	public static Ability voltAbsorb;
+	
+	public static Ability waterAbsorb;
+	public static Ability Oblivious;
+	public static Ability cloudNine;
+	public static Ability compoundEyes;
+	public static Ability Insomnia;
+	
+	public static Ability colorChange;
+	public static Ability Immunity;
+	public static Ability flashFire;
+	public static Ability shieldDust;
+	public static Ability ownTempo;
+	
+	public static Ability suctionCups;
+	public static Ability Intimidate;
+	public static Ability shadowTag;
+	public static Ability roughSkin;
+	public static Ability wonderGuard;
+	
 	public static Ability Levitate;
+	public static Ability effectSpore;
+	public static Ability Synchronize;
+	public static Ability clearBody;
+	public static Ability naturalCure;
+	
+	public static Ability lightningRod;
+	public static Ability sereneGrace;
+	public static Ability swiftSwim;
+	public static Ability Chlorophyll;
+	public static Ability Illuminate;
+	
+	public static Ability Trace;
+	public static Ability hugePower;
+	public static Ability poisonPoint;
+	public static Ability innerFocus;
+	public static Ability magmaArmor;
+	
+	public static Ability waterVeil;
+	public static Ability magnetPull;
+	public static Ability soundProof;
+	public static Ability rainDish;
+	public static Ability sandStream;
+	
+	public static Ability Pressure;
+	public static Ability thickFat;
+	public static Ability earlyBird;
+	public static Ability flameBody;
+	public static Ability runAway;
+	
+	public static Ability keenEye;
+	public static Ability hyperCutter;
+	public static Ability Pickup;
+	public static Ability Truant;
+	public static Ability Hustle;
+	
+	public static Ability cuteCharm;
+	public static Ability Plus;
+	public static Ability Minus;
+	public static Ability Forecast;
+	public static Ability stickyHold;
+	
+	public static Ability shedSkin;
+	public static Ability Guts;
+	public static Ability marvelScale;
+	public static Ability liquidOoze;
+	public static Ability Overgrow;
+	
+	public static Ability Blaze;
+	public static Ability Torrent;
+	public static Ability Swarm;
+	public static Ability rockHead;
+	public static Ability Drought;
+	
+	public static Ability arenaTrap;
+	public static Ability vitalSpirit;
+	public static Ability whiteSmoke;
+	public static Ability purePower;
+	public static Ability shellArmor;
+	
+	public static Ability airLock;
 	
 }
